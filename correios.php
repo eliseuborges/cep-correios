c420b4c54f115a122abfa4fc0cfec0e04c7d95f6
Eliseu Borges <contato@eliseuborges.com>
2017-07-02 13:26:03 -0300

    Correção da consulta e conversao para orientacao a objetos

---
+++ correios.php
@@ -0,0 +1,47 @@
+<?php
+/**
+ * Version: 1.5
+ * User: Eliseu Borges
+ * Date: 02/07/2017
+ * Time: 13:21
+ */
+
+class Correios{
+    public static function getEndereco($cep)
+    {
+
+        //Consulta
+        $action = "http://www.buscacep.correios.com.br/sistemas/buscacep/resultadoBuscaCepEndereco.cfm";
+        $ch = curl_init($action);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+
+        curl_setopt($ch, CURLOPT_POST, true);
+        curl_setopt($ch, CURLOPT_POSTFIELDS, "relaxation=" . $cep . "&Metodo=listaLogradouro&TipoConsulta=cep&StartRow=1&EndRow=10");
+
+        $response = curl_exec($ch);
+        curl_close($ch);
+
+
+        //Tratamento
+        $doc = new DOMDocument();
+        $doc->loadHTML($response);
+
+        $xpath = new \DOMXPath($doc);
+
+        $emptyCEP = $xpath->query('//p[contains(.,"DADOS NAO ENCONTRADOS")]')->length === 1;
+
+        if ($emptyCEP) {
+            return false;
+        }
+
+
+        $columns = $xpath->query('//table[@class="tmptabela"]/tr/td/text()');
+
+        return array(
+            "logradouro" => $columns[0]->nodeValue,
+            "bairro" => $columns[1]->nodeValue,
+            "cidadeUf" => $columns[2]->nodeValue,
+            "cep" => $columns[3]->nodeValue
+        );
+    }
+}
